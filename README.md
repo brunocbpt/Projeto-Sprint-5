c8d5c99cdd8d79a2d02897b65dd233b56f2188b2
brunocabacos <brunocabacos92@gmail.com>
2025-11-08 23:57:17 -0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # sprint-5
-Projeto Sprint 5
+
+# Projeto Sprint 5
+
+ # 📊 Anúncios de Vendas de Carros – Dashboard Interativo
+
+## Descrição:
+ Este é um aplicativo web desenvolvido com as bibliotecas: Pandas, Streamlit e Plotly Express para a vizualização de dados de vendas de carros.
+
+
+
+## Funcionalidades:
+- Upload de arquivos CSV contendo anúncios de veículos;
+- Criação de histogramas interativos;
+- Criação de gráficos de dispersão dinâmicos;
+- Interface simples e intuitiva para exploração de dados.
+
+## Tecnologias utilizadas:
+- Python
+- Streamlit
+- Plotly Express
+- Pandas
+- GitHub
+- Render
---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv ('vehicles.csv')
+
+st.header("Anúncios de venda de carros")
+hist_button = st.button('Criar histograma')
+
+if hist_button: #se o botão for clicado
+            # escrever uma mensagem
+            st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+            
+            # criar um histograma
+            fig = px.histogram(car_data, x="odometer")
+        
+            # exibir um gráfico Plotly interativo
+            st.plotly_chart(fig, use_container_width=True)
+
+scattered_button = st.button ('Criar gráfico de dispersão')
+
+if scattered_button:
+    st.write ('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.scatter(df, x="odometer")
+    st.plotly_chart (fig, use_container_width=True)
+
+
+
+build_histogram = st.checkbox ('Criar um histograma')
+
+if build_histogram:
+    st.write('Criando um histograma para a coluna odometer')
+    fig = px.histogram(df, x="odometer", y='price')
+    st.plotly_chart(fig, use_container_width=True)
